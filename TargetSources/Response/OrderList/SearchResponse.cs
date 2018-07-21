51a72b34349909be192f8f838b3419f22b470de3
kazenetu <rakudafactory@gmail.com>
2018-07-21 17:12:17 +0900

    対象CSプロジェクトを追加

---
+++ TargetSources/Response/OrderList/SearchResponse.cs
@@ -0,0 +1,29 @@
+﻿using DataTransferObjects.CustomTables;
+using Framework.DataTransferObject.BaseClasses;
+using System.Collections.Generic;
+
+namespace DataTransferObjects.Response.OrderList
+{
+  public class SearchResponse : ResponseBase<SearchResponse.SearchResponseParam>
+  {
+    public SearchResponse()
+    {
+    }
+
+    public SearchResponse(Results result, string errorMessage) : base(result, errorMessage)
+    {
+    }
+
+    public SearchResponse(Results result, string errorMessage, SearchResponseParam responseData) : base(result, errorMessage, responseData)
+    {
+    }
+
+    public class SearchResponseParam
+    {
+      /// <summary>
+      /// 検索結果
+      /// </summary>
+      public List<CustomTOrder> List { get; } = new List<CustomTOrder>();
+    }
+  }
+}
