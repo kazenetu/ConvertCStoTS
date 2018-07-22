a9968e66f55795ae290716d2b827047423b59ece
kazenetu <rakudafactory@gmail.com>
2018-07-22 15:09:45 +0900

    解析クラスを追加

---
+++ ConvertCStoTS/Converter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ConvertCStoTS
+{
+  public class Converter
+  {
+    private string SrcPath;
+    private string DescPath;
+
+    public Converter(string srcPath,string descPath)
+    {
+      SrcPath = srcPath;
+      DescPath = descPath;
+    }
+
+    public void ConvertTS(string targetFileName)
+    {
+      var codeAnalyzer = new CodeAnalyzer();
+
+      using(var sr = new StreamReader($"{SrcPath}/{targetFileName}"))
+      {
+        var sourceCode = codeAnalyzer.Analyze(sr.ReadToEnd());
+        Console.Write(sourceCode);
+      }
+    }
+  }
+}
