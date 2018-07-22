659f1170085a06565282ac57c5f9a04a893ae741
kazenetu <rakudafactory@gmail.com>
2018-07-22 22:14:59 +0900

    TypeScript変換確認用プロパティ追加

--- TargetSources/Response/OrderList/SearchResponse.cs
+++ TargetSources/Response/OrderList/SearchResponse.cs
@@ -24,6 +24,25 @@ namespace DataTransferObjects.Response.OrderList
       /// 検索結果
       /// </summary>
       public List<CustomTOrder> Results { get; } = new List<CustomTOrder>();
+
+      // TypeScript変換確認用
+      public DateTime dt{set;get;}
+      public Dictionary<int,int> test{set;}
+      public Dictionary<int,List<string>> test2{set;}
+
+      public int? a{set;get;}
+      public CustomTOrder? b{set;get;}
+      public int c{set;get;}=1;
+      public float c1{set;get;}
+      public double c2{set;get;}
+      public decimal c3{set;get;}
+      public bool bl{set;get;}
+
+      public string s{set;get;}="123";
+      public List<int> li{set;get;}=new List<int>();
+
+      public bool? nb{set;get;} = true;
+      public List<int?> nli{set;get;}=new List<int?>();
     }
   }
 }
