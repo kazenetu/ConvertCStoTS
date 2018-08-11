0d81d4c980c36c1b7352fdf22d2239a71aa95ee7
kazenetu <rakudafactory@gmail.com>
2018-08-12 00:27:36 +0900

    メソッド情報管理クラスを追加

---
+++ ConvertCStoTS/MethodData.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+
+namespace ConvertCStoTS
+{
+  /// <summary>
+  /// 変換中のメソッド情報
+  /// </summary>
+  public class MethodData
+  {
+    public int IndexSpaceCount { get; }
+
+    /// <summary>
+    /// スコープ
+    /// </summary>
+    public string Scope { get; }
+
+    /// <summary>
+    /// パラメータリスト情報
+    /// </summary>
+    public List<ParameterData> ParamList { get; }
+
+    /// <summary>
+    /// TypeScriptに変換したソースコード
+    /// </summary>
+    public string SourceCode { get; }
+
+    /// <summary>
+    /// パラメータ数
+    /// </summary>
+    public int PramCount
+    {
+      get
+      {
+        return ParamList.Count;
+      }
+    }
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="paramList">パラメータリスト情報</param>
+    /// <param name="sourceCode">TypeScriptに変換したソースコード</param>
+    public MethodData(int indexSpaceCount, string scope, List<ParameterData> paramList, string sourceCode)
+    {
+      IndexSpaceCount = indexSpaceCount;
+      Scope = scope;
+      ParamList = paramList;
+      SourceCode = sourceCode;
+    }
+
+    /// <summary>
+    /// パラメータ情報
+    /// </summary>
+    public class ParameterData
+    {
+      /// <summary>
+      /// パラメータ名
+      /// </summary>
+      public string Name { get; }
+
+      /// <summary>
+      /// パラメータの型
+      /// </summary>
+      public string LocalDeclaration { get; }
+
+      /// <summary>
+      /// コンストラクタ
+      /// </summary>
+      /// <param name="name">パラメータ名</param>
+      /// <param name="localDeclaration">パラメータの型</param>
+      public ParameterData(string name, string localDeclaration)
+      {
+        Name = name;
+        LocalDeclaration = localDeclaration;
+      }
+    }
+  }
+}
---
+++ ConvertCStoTS/MethodDataManager.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConvertCStoTS
+{
+  /// <summary>
+  /// TSメソッド管理クラス
+  /// </summary>
+  public class MethodDataManager
+  {
+    /// <summary>
+    /// メソッド名重複対応
+    /// </summary>
+    private readonly Dictionary<string, List<MethodData>> MethodLists = new Dictionary<string, List<MethodData>>();
+
+    /// <summary>
+    /// メソッドデータの追加
+    /// </summary>
+    /// <param name="methodName">メソッド名</param>
+    /// <param name="item">対象のメソッドデータ</param>
+    public void AddMethodData(string methodName,MethodData item)
+    {
+      if (!MethodLists.ContainsKey(methodName))
+      {
+        MethodLists.Add(methodName, new List<MethodData>());
+      }
+
+      MethodLists[methodName].Add(item);
+    }
+
+    /// <summary>
+    /// メソッドTSコードの取得
+    /// </summary>
+    /// <returns>メソッドTSコード</returns>
+    public string GetMethodText()
+    {
+      var result = new StringBuilder();
+
+      foreach (var methodName in MethodLists.Keys)
+      {
+        result.Append(CreateMethodText(methodName,MethodLists[methodName]));
+      }
+
+      return result.ToString();
+    }
+
+    /// <summary>
+    /// メソッド名単位のTSコード作成
+    /// </summary>
+    /// <param name="methodName">メソッド名</param>
+    /// <param name="methodDataList">メソッド名が同じメソッドコードリスト</param>
+    /// <returns></returns>
+    private string CreateMethodText(string methodName, List<MethodData> methodDataList)
+    {
+      var result = new StringBuilder();
+
+      var paramList = new List<HashSet<string>>();
+
+      var spaceIndex = "  ";
+      if (methodDataList.Count <= 1)
+      {
+        // メソッドが重複しない場合はそのまま出力
+        var item = methodDataList.FirstOrDefault();
+        if(item == null)
+        {
+          return string.Empty;
+        }
+
+        result.Append($"{spaceIndex}{item.Scope} {methodName}(");
+
+        var isFirst = true;
+        foreach(var param in item.ParamList)
+        {
+          if (!isFirst)
+          {
+            result.Append(", ");
+          }
+
+          result.Append(GetParam(param.Name, new List<string>() { param.LocalDeclaration }));
+
+          isFirst = false;
+        }
+
+        result.Append(")");
+        result.AppendLine(" {");
+
+        // メソッド内処理を変換
+        result.Append(item.SourceCode);
+
+        result.AppendLine(spaceIndex + "}");
+      }
+      else
+      {
+        // TODO 複数メソッド名が重複している場合
+       
+
+      }
+
+      return result.ToString();
+    }
+
+    /// <summary>
+    /// パラメータ設定
+    /// </summary>
+    /// <param name="name">パラメータ名</param>
+    /// <param name="localDeclarations">パラメータリスト</param>
+    /// <returns>パラメータ設定のTypeScriptソースコード</returns>
+    private string GetParam(string name,List<string> localDeclarations)
+    {
+      var result = new StringBuilder();
+
+      result.Append($"{name}");
+
+      var isFirst = true;
+      foreach(var localDeclaration in localDeclarations.Distinct())
+      {
+        if (isFirst)
+        {
+          result.Append(": ");
+          isFirst = false;
+        }
+        else
+        {
+          result.Append(" | ");
+        }
+
+        result.Append(localDeclaration);
+      }
+
+      return result.ToString();
+    }
+  }
+}
