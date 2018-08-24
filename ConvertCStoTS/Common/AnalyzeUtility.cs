276544af6c1b6781dfb41b9eafa7132367c5f78e
kazenetu <rakudafactory@gmail.com>
2018-08-24 23:28:51 +0900

    [WIP]メソッドコメント取得メソッドを追加

--- ConvertCStoTS/Common/AnalyzeUtility.cs
+++ ConvertCStoTS/Common/AnalyzeUtility.cs
@@ -2,7 +2,9 @@
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ConvertCStoTS.Common
 {
@@ -27,6 +29,16 @@ namespace ConvertCStoTS.Common
       {".ToString(",".toString(" },
       {".Length",".length" },
       {"int.Parse(","parseInt(" }
+    };
+
+    /// <summary>
+    /// 置き換え対象のコメントタグと置き換え文字
+    /// </summary>
+    private static Dictionary<string, string> CommentTagList = new Dictionary<string, string>()
+    {
+      {"<summary>{0}<\\/summary>","{0}" },
+      {"<param name=\"{0}\">{0}<\\/param>","@param {0} {1}" },
+      {"<remarks>{0}<\\/remarks>","@return {0}" },
     };
 
     /// <summary>
@@ -190,6 +202,68 @@ namespace ConvertCStoTS.Common
       var scopeKeyword = modifiers.Where(modifier => ScopeKeywords.Contains(modifier.ValueText));
 
       return string.Join(' ', scopeKeyword);
+    }
+
+    /// <summary>
+    /// コメント取得処理
+    /// </summary>
+    /// <param name="src">置き換え対象</param>
+    /// <returns>TypeScriptコメント</returns>
+    public static string GetComments(string src)
+    {
+      // 文字列が存在しない場合は改行だけを返す
+      if (string.IsNullOrEmpty(src.Trim()))
+      {
+        return Environment.NewLine;
+      }
+
+      var comments = src.Replace(Environment.NewLine, string.Empty, StringComparison.CurrentCulture);
+
+      foreach (var commentTag in CommentTagList)
+      {
+        comments = GetComment(comments, commentTag.Key, commentTag.Value);
+      }
+
+      string[] delimiter = { "///" };
+      var results = comments.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
+      if (results.Count() > 1)
+      {
+        var commentDetails = results.Where(item => !string.IsNullOrEmpty(item.Trim())).Select(item => "   * " + item.Trim()).ToList();
+        comments = $"  /** {Environment.NewLine}{string.Join(Environment.NewLine, commentDetails)}{Environment.NewLine}   */";
+      }
+      else
+      {
+        comments = $"  /** {results.ToString().Trim()} */";
+      }
+
+      return Environment.NewLine + comments + Environment.NewLine;
+
+      // コメント取得
+      string GetComment(string commentsText, string regexText, string replaceText)
+      {
+
+        var maches = Regex.Matches(commentsText, $"{string.Format(CultureInfo.CurrentCulture, regexText, "(.+?)")}");
+        if (maches.Count > 0)
+        {
+          var args = new List<string>();
+          foreach (Match match in maches)
+          {
+            if (match.Groups.Count < 2)
+            {
+              break;
+            }
+            for (var i = 1; i < match.Groups.Count; i++)
+            {
+              // カンマ区切りでパラメータリスト作成
+              args.Add(match.Groups[i].Value.Trim());
+            }
+            //commentsText = Regex.Replace(commentsText, string.Format(CultureInfo.CurrentCulture, regexText, ".*"), string.Format(CultureInfo.CurrentCulture, replaceText, args.ToArray()));
+            commentsText = Regex.Replace(commentsText, match.Groups[0].Value, string.Format(CultureInfo.CurrentCulture, replaceText, args.ToArray()));
+          }
+        }
+
+        return commentsText;
+      }
     }
 
   }
