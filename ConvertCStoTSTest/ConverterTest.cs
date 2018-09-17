3cece171e7ca8159b5b3145c3c674cabefe2e5c1
kazenetu <rakudafactory@gmail.com>
2018-09-17 09:12:05 +0900

    TestLogicクラス確認用テストケースを実装

--- ConvertCStoTSTest/ConverterTest.cs
+++ ConvertCStoTSTest/ConverterTest.cs
@@ -1,4 +1,4 @@
-using System;
+using ConvertCStoTS;
 using System.IO;
 using Xunit;
 
@@ -9,6 +9,23 @@ namespace ConvertCStoTSTest
     [Fact]
     public void TestLogicTest()
     {
+      var srcPath = "../../../../TargetSources";
+      var destPath = "./dist";
+      var isOutputMethod = true;
+      var filePath = "testlogic";
+      var otherReferencesPath = "base";
+
+      // TypeScriptファイル出力
+      var converter = new Converter(srcPath, destPath, isOutputMethod);
+      converter.ConvertTS($"{filePath}.cs", otherReferencesPath);
+
+      // ファイル読み込み
+      var expectedFilePath = "./ExpectedTypeScriptFiles/TestLogicFull.ts";
+      var expectedResult = ReadFile(expectedFilePath);
+      var outputFlieResult = ReadFile($"{destPath}/{filePath}.ts");
+
+      // ファイル比較
+      Assert.True(expectedResult == outputFlieResult, "変換エラー");
     }
 
     /// <summary>
