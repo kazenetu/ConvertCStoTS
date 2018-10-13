c4dea196a2a0e6883dd12bd8bfbe6d6dda8529d3
kazenetu <rakudafactory@gmail.com>
2018-10-13 17:34:13 +0900

    C#ファイルデータクラスを追加

---
+++ ConvertCStoTS/CSFileInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace ConvertCStoTS
+{
+  /// <summary>
+  /// C#ファイルデータ
+  /// </summary>
+  public class CSFileInfo
+  {
+    /// <summary>
+    /// 対象ファイルのディレクトリパス
+    /// </summary>
+    public string ImportPath { set; get; }
+
+    /// <summary>
+    /// ソースファイル
+    /// </summary>
+    public string SourceCode { set; get; }
+  }
+}
