958465a3c9c63988ace75f7c9aad0cad4ee15ef7
kazenetu <rakudafactory@gmail.com>
2018-10-15 19:37:15 +0900

    クラスメソッドを追加

--- TargetSources/OtherClass.cs
+++ TargetSources/OtherClass.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 
 namespace DataTransferObjects
 {
-    public class OtherClass{
-        public static string StaticField = "456";
+  public class OtherClass
+  {
+    public class InnerClass
+    {
+      public static string StaticField = "789";
 
-        public class InnerClass
-        {
-            public static string StaticField = "111";
-        }
+      public static string StaticMethod()
+      {
+        return "aaa";
+      }
+
+      public static string StaticMethodArg(string name)
+      {
+        return "hellow!" + name;
+      }
     }
+    public static string StaticField = "456";
+    public static string StaticMethod()
+    {
+      return "bbb";
+    }
+  }
 }
