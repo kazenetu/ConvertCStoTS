14f457d77d436fa3fe87c323ed45752b981636e2
kazenetu <rakudafactory@gmail.com>
2018-10-15 18:42:51 +0900

    外部参照用クラスを追加

---
+++ TargetSources/OtherClass.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace DataTransferObjects
+{
+    public class OtherClass{
+        public static string StaticField = "456";
+
+        public class InnerClass
+        {
+            public static string StaticField = "111";
+        }
+    }
+}
