adfae208fd36baad74190217ff361103fd38dd32
kazenetu <rakudafactory@gmail.com>
2018-11-01 23:19:09 +0900

    設計見直し用C#ファイル解析クラスを追加

---
+++ ConvertCStoTS/CSharpAnalyze/Analyze.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpAnalyze
+{
+  /// <summary>
+  /// C#解析クラス
+  /// </summary>
+  public class Analyze
+  {
+  }
+}
