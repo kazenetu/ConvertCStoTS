bea494a6b835c19e2ca75434d3f9d6fa4b74ac78
kazenetu <rakudafactory@gmail.com>
2018-11-04 16:27:59 +0900

    C#ファイル読み込み処理を実装

--- ConvertCStoTS/CSharpAnalyze/Analyze.cs
+++ ConvertCStoTS/CSharpAnalyze/Analyze.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using CSharpAnalyze.Repositories;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace CSharpAnalyze
@@ -9,5 +14,55 @@ namespace CSharpAnalyze
   /// </summary>
   public class Analyze
   {
+    #region コンストラクタ
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="rootPath">ルートパス</param>
+    public Analyze(string rootPath)
+    {
+      // セマンティックモデルのリストを作成する
+      if (!CreateModels(rootPath))
+      {
+        // TODO エラー
+      }
+
+      // TODO 解析処理
+    }
+    #endregion
+
+    #region インスタンスフィールド
+    /// <summary>
+    /// セマンティックモデルリスト
+    /// </summary>
+    private List<SemanticModel> Models = new List<SemanticModel>();
+    #endregion
+
+    /// <summary>
+    /// セマンティックモデルを作成する
+    /// </summary>
+    /// <param name="rootPath">ルートパス</param>
+    /// <returns>作成結果</returns>
+    private bool CreateModels(string rootPath)
+    {
+      // C#ファイル情報リストを取得
+      var csFileInfos = CSFileInfo.GetCSFileInfoList(rootPath);
+
+      // 編集クラスを作成
+      var mscorlib = MetadataReference.CreateFromFile(typeof(object).GetTypeInfo().Assembly.Location);
+      var treeList = csFileInfos.Select(info => CSharpSyntaxTree.ParseText(info.SourceCode)).ToList();
+      var compilation = CSharpCompilation.Create("", treeList, new[] { mscorlib });
+
+      // セマンティックモデルのリストを取得する
+      foreach (var model in treeList)
+      {
+        Models.Add(compilation.GetSemanticModel(model));
+      }
+
+      
+
+      return false;
+    }
+
   }
 }
