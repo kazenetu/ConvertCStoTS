74fe822384a44649347a3afe67563023788cfaa2
kazenetu <rakudafactory@gmail.com>
2018-11-07 22:02:36 +0900

    セマンティックモデルクラスを呼び出す処理を実装

--- ConvertCStoTS/CSharpAnalyze/Analyze.cs
+++ ConvertCStoTS/CSharpAnalyze/Analyze.cs
@@ -22,20 +22,22 @@ namespace CSharpAnalyze
     public Analyze(string rootPath)
     {
       // セマンティックモデルのリストを作成する
-      if (!CreateModels(rootPath))
+      var models = CreateModels(rootPath);
+      if (!models.Any())
       {
         // TODO エラー
       }
 
       // TODO 解析処理
+      foreach(var model in models)
+      {
+        if (model.SyntaxTree.FilePath.Contains("TestLogic"))
+        {
+          var sma = new SemanticModelAnlayze(model);
+          break;
+        }
+      }
     }
-    #endregion
-
-    #region インスタンスフィールド
-    /// <summary>
-    /// セマンティックモデルリスト
-    /// </summary>
-    private List<SemanticModel> Models = new List<SemanticModel>();
     #endregion
 
     /// <summary>
@@ -43,25 +45,23 @@ namespace CSharpAnalyze
     /// </summary>
     /// <param name="rootPath">ルートパス</param>
     /// <returns>作成結果</returns>
-    private bool CreateModels(string rootPath)
+    private List<SemanticModel> CreateModels(string rootPath)
     {
       // C#ファイル情報リストを取得
       var csFileInfos = CSFileInfo.GetCSFileInfoList(rootPath);
 
       // 編集クラスを作成
       var mscorlib = MetadataReference.CreateFromFile(typeof(object).GetTypeInfo().Assembly.Location);
-      var treeList = csFileInfos.Select(info => CSharpSyntaxTree.ParseText(info.SourceCode)).ToList();
+      var treeList = csFileInfos.Select(info => CSharpSyntaxTree.ParseText(info.SourceCode, null, info.RelativePath)).ToList();
       var compilation = CSharpCompilation.Create("", treeList, new[] { mscorlib });
 
       // セマンティックモデルのリストを取得する
+      var models = new List<SemanticModel>();
       foreach (var model in treeList)
       {
-        Models.Add(compilation.GetSemanticModel(model));
+        models.Add(compilation.GetSemanticModel(model));
       }
-
-      
-
-      return false;
+      return models;
     }
 
   }
