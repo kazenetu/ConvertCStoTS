f48ad9f8852cf518f6adc0507a8bb38c22ecc040
kazenetu <rakudafactory@gmail.com>
2018-11-09 22:20:34 +0900

    C#ファイルモデルクラス追加

---
+++ ConvertCStoTS/CSharpAnalyze/DomainModel/CSFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpAnalyze.DomainModel
+{
+  public class CSFile
+  {
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="relativePath">相対パス</param>
+    /// <param name="sourceCode">ソースコード</param>
+    public CSFile(string relativePath, string sourceCode)
+    {
+      RelativePath = relativePath;
+      SourceCode = sourceCode;
+    }
+
+    #region インスタンスプロパティ
+
+    /// <summary>
+    /// 対象ファイルの相対ディレクトリパス
+    /// </summary>
+    public string RelativePath { get; }
+
+    /// <summary>
+    /// ソースファイル
+    /// </summary>
+    public string SourceCode { get; }
+
+    #endregion
+  }
+}
