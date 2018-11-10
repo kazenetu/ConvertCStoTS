e868d1763e840ccd4e1245102b57b90b4142ea69
kazenetu <rakudafactory@gmail.com>
2018-11-10 17:26:18 +0900

    C#ファイルファイル読み込みクラスを追加

---
+++ ConvertCStoTS/CSharpAnalyze/Infrastructure/ICSFileRepository.cs
@@ -0,0 +1,19 @@
+﻿using CSharpAnalyze.DomainModel;
+using System.Collections.Generic;
+
+namespace CSharpAnalyze.Infrastructure
+{
+  /// <summary>
+  /// C#ファイルデータ読み込み
+  /// </summary>
+  public interface ICSFileRepository
+  {
+    /// <summary>
+    /// C#ファイルデータリストを取得する
+    /// </summary>
+    /// <param name="rootPath">ルートパス</param>
+    /// <param name="exclusionKeywords">除外フォルダリスト</param>
+    /// <returns>C#ファイルデータのリスト</returns>
+    List<(string relativePath, string source)> GetCSFileList(string rootPath, List<string> exclusionKeywords);
+  }
+}
