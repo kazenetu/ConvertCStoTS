d0a76e6d088091c03396565151eb46e348c0cfb3
kazenetu <rakudafactory@gmail.com>
2018-11-15 20:30:20 +0900

    C#解析アプリケーションレイヤを追加

---
+++ ConvertCStoTS/CSharpAnalyze/ApplicationService/AnalyzeApplication.cs
@@ -0,0 +1,24 @@
+﻿using CSharpAnalyze.Domain.Model;
+using CSharpAnalyze.Domain.Service;
+using CSharpAnalyze.Infrastructure;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpAnalyze.ApplicationService
+{
+  /// <summary>
+  /// C#解析アプリケーション
+  /// </summary>
+  /// <remarks>リクエストとレスポンスの変換アダプタなどを行う</remarks>
+  public class AnalyzeApplication
+  {
+    public List<SemanticModelAnlayze> GetAnalyzeResult(string rootPath)
+    {
+      var fileRepository = new CSFileRepository();
+      var analizeService = new AnalyzeService(rootPath, fileRepository);
+
+      return analizeService.GetAnalyzeResult();
+    }
+
+  }
+}
