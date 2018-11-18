2c12d9dbd26029f481dc5978a1d75210a0d09009
kazenetu <rakudafactory@gmail.com>
2018-11-18 18:35:38 +0900

    C#ファイル解析結果 詳細情報インターフェース追加

---
+++ ConvertCStoTS/CSharpAnalyze/Domain/Model/Analyze/Items/ISemanticModelAnalyzeItem.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace ConvertCStoTS.CSharpAnalyze.Domain.Model.Analyze.Items
+{
+  /// <summary>
+  /// C#ファイル解析結果 詳細情報
+  /// </summary>
+  public interface ISemanticModelAnalyzeItem
+  {
+    /// <summary>
+    /// 子メンバ
+    /// </summary>
+    List<ISemanticModelAnalyzeItem> Members { get; }
+
+    /// <summary>
+    /// アイテム種別
+    /// </summary>
+    ItemTypes ItemType { get; }
+
+    /// <summary>
+    /// 名前
+    /// </summary>
+    string Name { get; }
+
+    /// <summary>
+    /// 修飾子リスト
+    /// </summary>
+    List<string> Modifiers { get; }
+
+    /// <summary>
+    /// コメント
+    /// </summary>
+    List<string> Comments { get; }
+
+    /// <summary>
+    /// 文字列取得
+    /// </summary>
+    /// <returns>文字列</returns>
+    string ToString();
+  }
+
+  /// <summary>
+  /// アイテム種別
+  /// </summary>
+  public enum ItemTypes
+  {
+    Class,
+    Field,
+    Property,
+    Method,
+    MethodStatement
+  }
+}
