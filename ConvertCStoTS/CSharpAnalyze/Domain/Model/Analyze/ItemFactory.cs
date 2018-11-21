6eb6d13bc13916fb9eda63e3110590fbd3c4296d
kazenetu <rakudafactory@gmail.com>
2018-11-21 20:34:23 +0900

    ISemanticModelAnalyzeItemインスタンス作成クラス追加

---
+++ ConvertCStoTS/CSharpAnalyze/Domain/Model/Analyze/ItemFactory.cs
@@ -0,0 +1,35 @@
+﻿using ConvertCStoTS.CSharpAnalyze.Domain.Model.Analyze.Items;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace ConvertCStoTS.CSharpAnalyze.Domain.Model.Analyze
+{
+  /// <summary>
+  /// ISemanticModelAnalyzeItemインスタンス作成クラス
+  /// </summary>
+  public static class ItemFactory
+  {
+
+    /// <summary>
+    /// エントリメソッド
+    /// </summary>
+    /// <param name="node">対象Node</param>
+    /// <param name="target">対象ソースのsemanticModel</param>
+    /// <returns>ISemanticModelAnalyzeItemインスタンス</returns>
+    public static ISemanticModelAnalyzeItem Create(SyntaxNode node, SemanticModel semanticModel)
+    {
+      return Create((dynamic)node, semanticModel);
+    }
+
+    /// <summary>
+    /// クラスアイテム作成
+    /// </summary>
+    /// <param name="node">対象Node</param>
+    /// <param name="target">対象ソースのsemanticModel</param>
+    /// <returns>ISemanticModelAnalyzeItemインスタンス</returns>
+    private static ISemanticModelAnalyzeItem Create(ClassDeclarationSyntax node, SemanticModel semanticModel)
+    {
+      return new ItemClass(node, semanticModel);
+    }
+  }
+}
