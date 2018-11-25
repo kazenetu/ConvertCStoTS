928c20514ddab97b8ec9f3c6e4cf767a16c0ba83
kazenetu <rakudafactory@gmail.com>
2018-11-25 16:32:38 +0900

    C#解析完了イベント追加

---
+++ ConvertCStoTS/CSharpAnalyze/Domain/Event/Analyze/Analyzed.cs
@@ -0,0 +1,24 @@
+﻿using ConvertCStoTS.CSharpAnalyze.Domain.Model.Analyze.Items;
+
+namespace ConvertCStoTS.CSharpAnalyze.Domain.Event.Analyze
+{
+  /// <summary>
+  /// 解析完了イベント
+  /// </summary>
+  public class Analyzed : IEvent
+  {
+    public string FilePath { get; }
+    public ISemanticModelAnalyzeItem AnalyzeResult { get; }
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="filepath">ファイルパス</param>
+    /// <param name="analyzeResult">解析結果</param>
+    public Analyzed(string filepath, ISemanticModelAnalyzeItem analyzeResult)
+    {
+      FilePath = filepath;
+      AnalyzeResult = analyzeResult;
+    }
+  }
+}
