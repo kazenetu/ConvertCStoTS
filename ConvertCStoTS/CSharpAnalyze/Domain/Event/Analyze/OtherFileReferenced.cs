6d5e422d87691ee150e0a0e852c9155a6de755f4
kazenetu <rakudafactory@gmail.com>
2018-12-01 18:29:35 +0900

    外部ファイル参照イベント追加

---
+++ ConvertCStoTS/CSharpAnalyze/Domain/Event/Analyze/OtherFileReferenced.cs
@@ -0,0 +1,29 @@
+﻿namespace ConvertCStoTS.CSharpAnalyze.Domain.Event.Analyze
+{
+  /// <summary>
+  /// 外部ファイル参照イベント
+  /// </summary>
+  public class OtherFileReferenced : IEvent
+  {
+    /// <summary>
+    /// 外部ファイルパス
+    /// </summary>
+    public string FilePath { get; }
+    
+    /// <summary>
+    /// 参照クラス名
+    /// </summary>
+    public string ClassName { get; }
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="filePath">外部ファイルパス</param>
+    /// <param name="className">参照クラス名</param>
+    public OtherFileReferenced(string filePath,string className)
+    {
+      FilePath = filePath;
+      ClassName = className;
+    }
+  }
+}
